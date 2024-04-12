33b11d53a0b1ab6f8e4c53064ffc6ef88cab5564
jereldlimjy <jereldlimjy@hotmail.com>
2024-04-12 22:52:09 +0800

    update cache

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,8 @@ export async function generateMetadata() {
 const getLeaderboard = async () => {
     try {
         const response = await fetch(
-            "https://sg-searchers.vercel.app/api/leaderboard"
+            "https://sg-searchers.vercel.app/api/leaderboard",
+            { cache: "no-store" }
         );
         if (!response.ok) {
             throw new Error("Network response was not ok");
