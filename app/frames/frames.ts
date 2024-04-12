712b8e24799088c65339d579b49d02b3557f87eb
jereldlimjy <jereldlimjy@hotmail.com>
2024-04-12 17:30:46 +0800

    update

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,8 +1,5 @@
 import { createFrames } from "frames.js/next";
 import { farcasterHubContext } from "frames.js/middleware";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 export const frames: any = createFrames({
     basePath: "/frames",
