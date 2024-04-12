95b575f249e8ad4cf9c1abdd6b64d5ec96394ffe
jereldlimjy <jereldlimjy@hotmail.com>
2024-04-12 17:12:17 +0800

    updates

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -6,21 +6,5 @@ dotenv.config();
 
 export const frames: any = createFrames({
     basePath: "/frames",
-    middleware: [
-        farcasterHubContext({
-            ...(process.env.NODE_ENV === "production"
-                ? {
-                      hubHttpUrl: "https://hubs.airstack.xyz",
-                      hubRequestOptions: {
-                          headers: {
-                              "x-airstack-hubs": process.env
-                                  .AIRSTACK_API_KEY as string,
-                          },
-                      },
-                  }
-                : {
-                      hubHttpUrl: "http://localhost:3010/hub",
-                  }),
-        }),
-    ],
+    middleware: [farcasterHubContext()],
 });
